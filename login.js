2f899968ebd1eb4fef38f1270799683139d9b34f
maheshwarivaibhav-web <v.s.maheshwari1998@gmail.com>
2021-01-21 15:25:56 +0530

    fourth Commit For Test(21-01-2021)

--- login.js
+++ login.js
@@ -1,19 +1,21 @@
 //login button action
 document.getElementById('login').addEventListener('click',()=>{
 
+    //get the values from login form
     let email = document.getElementById('lemail').value;
     let password = document.getElementById('lpassword').value;
 
-    if (email === '' || password === '') 
+    if (email === '' || password === '') //check field is empty or not
     {
         document.getElementById('loginmsg').textContent = "All Fields are required!!!";
     }
     else
     {
         let adminUser = JSON.parse(localStorage.getItem('adminUser'));
-        if (adminUser.email === email && adminUser.password === password) {
+        if (adminUser.email === email && adminUser.password === password) { //for admin user
            window.location = 'dashboard.html'; 
 
+           //set login session to localstorage
            let userSession = {
                name : adminUser.name,
                loginDateTime : new Date().toLocaleString()//current date time
@@ -22,7 +24,37 @@ document.getElementById('login').addEventListener('click',()=>{
         } 
         else 
         {
-        
+            //for sub user
+            let userData = JSON.parse(localStorage.getItem('users'));
+            let loginflag = false;
+            let logUser = {};
+            for (const user of userData) {
+                loginflag = false;
+                if (user.email === email && user.password === password) {
+                    loginflag = true; 
+                    logUser = user;
+                    break;
+                }
+                
+            }
+
+            ///check user is existor not
+            if (loginflag) {
+
+                //set login session to localstorage
+                let userSession = {
+                    name : logUser.name,
+                    loginDateTime : new Date().toLocaleString()//current date time
+                };
+                localStorage.setItem('userSession',JSON.stringify(userSession));
+                
+                //redirect to subuser.html page
+                window.location = 'subuser.html'; 
+
+            }
+            else{
+                document.getElementById('loginmsg').textContent = "Wrong Email or password !!!";
+            }
         }
     }
 });
--- main.js
+++ main.js
@@ -1,9 +1,14 @@
+//check user is loged in or not 
+let logUser = JSON.parse(localStorage.getItem('userSession'));
 
+if (logUser === null) {
+    window.location = 'login.html'; //if user is not login then redirect yo login page
+}
 //get login user details
-document.getElementById('loguser').textContent = `Hello, ${JSON.parse(localStorage.getItem('userSession')).name}`;
+document.getElementById('loguser').textContent = `Hello, ${logUser.name}`;
 
 
-
+// logout function
 function logout() {
     let sessionData = [];
 
@@ -11,16 +16,52 @@ function logout() {
 
     if(localstorageData)
     {
-        sessionData = localStorage;
+        sessionData = localstorageData;
     }
 
     let userSession = JSON.parse(localStorage.getItem('userSession'));
     userSession.logoutDateTime = new Date().toLocaleString(); //current date time
     sessionData.push(userSession);
 
-    localStorage.setItem('sessionData',JSON.stringify(sessionData));
-    localStorage.removeItem('userSession');
-    window.location = 'login.html';
+    localStorage.setItem('sessionData',JSON.stringify(sessionData)); //set login session data
+    localStorage.removeItem('userSession'); // unset session data
+    window.location = 'login.html'; // redirect ro login page
+
+}
+
+//get user session data
+
+function loadUserSessionData() {
+    let userdata = JSON.parse(localStorage.getItem('sessionData'));
+    let data = '';
+    if (userdata) {
+        data = '<table class="table" border=1><thead class="thead-dark"><tr><th>Name</th><th>Login Date Time</th><th>Logout Date Time</th></tr></thead>';
+        for (const x of userdata) {
+            data = `${data} <tr><td>${x.name}</td><td>${x.loginDateTime}</td><td>${x.logoutDateTime}</td></tr>`;
+        }
+        data = `${data} </table>`;
+        
+    } else 
+    {
+        data = '<h3>No Record Found!!!</h3>';
+    }
+    document.getElementById('usersessiondata').innerHTML = data;
+}
 
 
+function checkBirthDate() {
+    let userdata = JSON.parse(localStorage.getItem('users'));
+
+
+    let user = userdata.filter((user) => user.name === logUser.name);
+
+    let date  = Number(user[0].dob.substring(10,8));
+    let month = Number(user[0].dob.substring(7,5));
+    
+    if (date == new Date().getDay() && month == (new Date().getMonth()+1)) 
+    {
+        document.getElementById('birthdaywish').innerHTML = '<h2 class="error text-center"> Happy Birthday !</h2>';
+    }
+    
+    
 }
